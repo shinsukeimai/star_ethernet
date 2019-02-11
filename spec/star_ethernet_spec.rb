1ef3f912f31fe91a9c0f0fae4eb512016a83deab
Shinsuke IMAI <imaishinsuke@gmail.com>
2019-02-11 15:34:23 +0900

    Add travis ci (#4)
    * Add travis ci
    
    * Remove inital fail test
    
    * Add travis status to readme
    
    * Add contribution to read me

--- spec/star_ethernet_spec.rb
+++ spec/star_ethernet_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe StarEthernet do
   it "has a version number" do
     expect(StarEthernet::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
